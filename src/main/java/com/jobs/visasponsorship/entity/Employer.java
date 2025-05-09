307d5f1bde541b3ad7c163b0e6085356f9a8c6ec
kailan <Kailan1001101@gmail.com>
2025-05-09 23:47:16 +0200

    Employer API Get Post

--- src/main/java/com/jobs/visasponsorship/entity/Employer.java
+++ src/main/java/com/jobs/visasponsorship/entity/Employer.java
@@ -1,4 +1,87 @@
 package com.jobs.visasponsorship.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
 public class Employer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String companyName;
+    private String website;
+    private String industry;
+    private String location;
+
+    public Employer(){
+
+    }
+
+    public Employer(Long id, String companyName, String website, String industry, String location, Boolean verified, Date firstDate) {
+        this.id = id;
+        this.companyName = companyName;
+        this.website = website;
+        this.industry = industry;
+        this.location = location;
+        this.verified = verified;
+        this.firstDate = firstDate;
+    }
+
+    private Boolean verified;
+    private Date firstDate;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Boolean getVerified() {
+        return verified;
+    }
+
+    public void setVerified(Boolean verified) {
+        this.verified = verified;
+    }
+
+    public Date getFirstDate() {
+        return firstDate;
+    }
+
+
+
+
+
+    
 }
--- src/main/java/com/jobs/visasponsorship/service/EmployerService.java
+++ src/main/java/com/jobs/visasponsorship/service/EmployerService.java
@@ -1,4 +1,31 @@
 package com.jobs.visasponsorship.service;
 
+import com.jobs.visasponsorship.entity.Employer;
+import com.jobs.visasponsorship.repo.EmployerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EmployerService {
+
+    private final EmployerRepository employerRepository;
+
+
+    public EmployerService(EmployerRepository employerRepository) {
+        this.employerRepository = employerRepository;
+
+
+    }
+
+    public List<Employer> getAllEmployers(){
+        return employerRepository.findAll();
+    }
+
+    public void insertEmployer(Employer employer) {
+        employerRepository.save(employer);
+    }
+
 }
+
+
